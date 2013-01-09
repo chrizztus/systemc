89fb2f6e67f64d7033636d13ecaa3085ea419145
Christian Haake <chaake@myBook.local>
2013-01-09 12:58:39 +0100

    Initial Commit

---
+++ SystemCTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SystemCTest
+//
+//  Created by Christian Haake on 1/9/13.
+//  Copyright (c) 2013 baroos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
